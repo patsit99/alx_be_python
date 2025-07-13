9f2fa9d9db7a7ae29a5a72aa74c8055a55adcd17
patsit99 <patee.t11@gmail.com>
2025-07-13 21:40:16 +0200

    Add files via upload

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,12 +1,12 @@
 # Simple Interest Forumula
 #I = P*R*T
 
-princpal = 1000     # Initial investment in dollars
+principal = 1000     # Initial investment in dollars
 rate = 0.05         # Annual interest rate (5%)
 time = 3            # Time in years
 
 # Perform arthimetic 
-interest = princpal * rate * time
+interest = principal * rate * time
 
 # Print the result
 print(f"The simple interest is {interest}")
